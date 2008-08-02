33569a0539613f2a43f8d6201d50a5cced5f83fa
Aman Gupta <aman@tmm1.net>
2008-08-02 01:59:52 -0700

    more symbol monkeypatching: support for #asc, #desc and #in

--- rmongo.rb
+++ rmongo.rb
@@ -173,18 +173,29 @@ end
 # XXX this is terrible, yes i know!
 class Symbol
 
-  {
-    :<  => :$lt,
+  { :<  => :$lt,
     :<= => :$lte,
     :>  => :$gt,
-    :>= => :$gte
-  }.each do |func, key|
-    class_eval %[
-      def #{func} num
-        { self => { :#{key} => num }}
-      end
-    ]
+    :>= => :$gte }.each do |func, key|
+                     class_eval %[
+                       def #{func} num
+                         { self => { :#{key} => num }}
+                       end
+                     ]
+                   end
+
+  def in values
+    { self => { :$in => values }}
   end
+
+  def asc
+    { self => 1 }
+  end
+  
+  def desc
+    { self => -1 }
+  end
+
 end
 
 EM.run{
