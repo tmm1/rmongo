6b61031b891821db3d1186bd6cac092510ed075e
Aman Gupta <aman@tmm1.net>
2008-08-02 01:57:55 -0700

    more run.sh cleanups

--- jruby/run.sh
+++ jruby/run.sh
@@ -2,18 +2,20 @@
 export TENGEN_APPSERVER=`expr "$(cat 10gen.properties)" : 'BASE=\(.*\)\/libraries'`
 echo TENGEN_APPSERVER=$TENGEN_APPSERVER
 
-TEMP_CLASSPATH=./bin/:$TENGEN_APPSERVER/build/
+CLASSPATH=./bin/:$TENGEN_APPSERVER/build/
 for i in $TENGEN_APPSERVER/include/*.jar; do
-  export TEMP_CLASSPATH=${TEMP_CLASSPATH}:${i}
+  export CLASSPATH=${CLASSPATH}:${i}
 done
-echo TEMP_CLASSPATH=$TEMP_CLASSPATH
+echo CLASSPATH=$CLASSPATH
 
 export ED_HOME=$TENGEN_APPSERVER
 echo ED_HOME=$TENGEN_APPSERVER
 
 echo Ready to run mongo.rb
+
 #javac -d bin src/mongo/ruby/Mongo.java
 #java mongo.ruby.Mongo
-CLASSPATH=$TEMP_CLASSPATH jruby mongo.rb
+CLASSPATH=$CLASSPATH jruby mongo.rb
+#CLASSPATH=$CLASSPATH jruby -S irb -r mongo.rb
 
 echo Done.
